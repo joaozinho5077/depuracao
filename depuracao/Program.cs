be091240b9ec3e502d6fae3a0d753b8a1aea00f1
Joao Cicero <jc_ifsp@hotmail.com>
2012-08-16 21:21:35 -0300

    comitadinha inicial

---
+++ depuracao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace depuracao
+{
+    class Program
+    {
+        static int Mudar(int n)
+        {
+            n = n + 1;
+            return n;
+        }
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("O numero escolhido é: {0}", numero);
+            numero = Mudar(numero);
+            Console.WriteLine("O numero modificado é: {0}", numero);
+            
+            Console.Read();
+        }
+    }
+}
